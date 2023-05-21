05e0bbdeb8fa213919bb4c22d8bbd420f595dfa8
Ani2396 <ani.sargsyan@davinci.edu.ar>
2023-05-21 19:53:13 -0300

    adding controllers and models package

---
+++ MFGAweb/MFGAweb/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace MFGAweb.Models
+{
+    public class User
+    {
+        public string user { get; set; }
+        public string password { get; set; }
+
+        public User(string user, string pass)
+        {
+            this.user = user;
+            this.password = pass;
+        }
+    }
+}
