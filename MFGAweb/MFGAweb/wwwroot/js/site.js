1ef2e87271b703f76d58c855624841fdecadf469
Seba-Peitiado <sebastian.peitiado@davinci.edu.ar>
2023-06-01 19:04:33 -0300

    Update site.js
    Codigo para Registro

--- MFGAweb/MFGAweb/wwwroot/js/site.js
+++ MFGAweb/MFGAweb/wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.getElementById("formularioRegistro").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    var email = document.getElementById("email").value;
+    var contrasena = document.getElementById("contrasena").value;
+    var confirmarContrasena = document.getElementById("confirmarContrasena").value;
+  
+    if (email === "" || contrasena === "" || confirmarContrasena === "") {
+      alert("Por favor, complete todos los campos.");
+      return;
+    }
+  
+    if (contrasena !== confirmarContrasena) {
+      alert("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.");
+      return;
+    }
+  
+    // Aquí puedes agregar la lógica para enviar los datos de registro al servidor o realizar otras acciones
+  
+    alert("Registro exitoso. ¡Bienvenido!");
+  
+    // Restablecer los campos del formulario
+    document.getElementById("email").value = "";
+    document.getElementById("contrasena").value = "";
+    document.getElementById("confirmarContrasena").value = "";
+  });
+  
