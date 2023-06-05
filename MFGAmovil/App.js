0509e100334f13912885864794fe5f5fb77f97b3
Gonzalo <gonzalo.acero@davinci.edu.ar>
2023-06-04 21:00:24 -0300

    Agregado de imagenes (logo)

--- MFGAmovil/App.js
+++ MFGAmovil/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <LoginScreen />
       </View>
     );
-  }
-}
+  };
+};
 
 export default App;
---
+++ MFGAmovil/Login/Logo.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import { View, Image } from "react-native/types";
+import Styles from '../Styles/Styles'
+
+
+class Logo extends Component {
+    render(){
+        return(
+            <View>
+
+            </View>
+        );
+    };
+};
+
+
+export default Logo;
